bd7b173f9f992810313977a5ab82581a596fcb71
Konrad Gryka <conradgry@gmail.com>
2021-03-10 22:54:30 +0100

    Pierwszy kommit

---
+++ FirstTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

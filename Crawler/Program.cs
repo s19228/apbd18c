7e1ca31ec48ce624ca59a172df82d2464b3075fd
Konrad Gryka <conradgry@gmail.com>
2021-03-12 21:05:26 +0100

    Crawler final

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var url = args[0];
+
+            // po wykonaniu kodu zwalnia zasoby które zostały przyznane httpClientowi
+
+            using (var httpClient = new HttpClient()) 
+            {
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+                    var emailAdresses = regex.Matches(content);
+
+
+                    foreach (var match in emailAdresses)
+                    {
+                        System.Console.WriteLine(match.ToString());
+                    }
+
+                }
+            };
+
+
+
+
+
         }
     }
 }
+ 

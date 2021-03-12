920a106dfbfaf4b9ee6dee73e40356132215c4e2
Konrad Gryka <conradgry@gmail.com>
2021-03-12 20:03:41 +0100

    Clrawler - testowy komit

--- FirstTutorial/Program.cs
+++ FirstTutorial/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstTutorial
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            // tego w javie nie ma ale można stosować konstuktory jak w javie
+            Student stud = new Student
+            {
+                FirstName = "Daniel",
+                Age = 12 
+            };
+
+            var stud2 = new Student("Janek", 23);
+             
+
+            //snipet cw
+            //konkatenacja stringów tzw interpolacja stringa $
+            Console.WriteLine($"Imie studenta to: {stud.FirstName} i ma {stud.Age} lat");
+
+            Console.WriteLine(stud2.ToString());
+
+            //ICollection - możemy coś wstawiać
+            //INumerable - tylko iterowanie
+
+            var list = new List<string> {"Ala", "ma", "kota"};
+
+            foreach (var wrt in list)
+            {
+                Console.WriteLine(wrt);
+            }
+
+
+ 
+
         }
     }
 }
---
+++ FirstTutorial/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstTutorial
+{
+    public class Student
+    {
+        // settery i gettery 
+        // prop
+
+        public string FirstName { get; set; }
+
+        // propfull
+
+        public int Age { get; set; }
+
+        public String ToString()
+        {
+            return FirstName + " " + Age;
+        }
+
+        public Student(String Name, int Age) 
+        {
+            this.FirstName = Name;
+            this.Age = Age;
+        
+        }
+
+        public Student()
+        {
+        
+        }
+
+
+    }
+}
